dc344ad1eee08f4483e9a8ae84d9e2ecec5e8dd8
Galina Mikhaylova <gala6858@mail.ru>
2023-06-12 21:18:15 +0300

    change appveyor

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -57,39 +57,18 @@ class MoneyTransferTest {
     }
 
 //    @Test
-//    void shouldTransferMoneyBetweenOwnCardsFromSecondCard() {
-//        open("http://localhost:9999");
-//        var loginPage = new LoginPage(); //создаем экземпляр странички LoginPage
-////    var loginPage = open("http://localhost:9999", LoginPageV1.class);
-//        var authInfo = DataHelper.getAuthInfo();//готовим данные для тестов, используя DataHelper проверяем успешный вход
-//        // метод getAuthInfo, он нам возвращает объект с данными и мы их передаем далее в validLogin
-//        var verificationPage = loginPage.validLogin(authInfo);
-//        var verificationCode = DataHelper.getVerificationCodeFor(authInfo); //получаем VerificationCode
-//        verificationPage.validVerify(verificationCode); //на verificationPage вводим код
-//        var transferPage = new TransferPage();
-//        transferPage.replenishInFirst();
-//    }
-//
-//    @Test
-//    void shouldTransferMoneyBetweenOwnCardsFromFirstCard() {
-//        open("http://localhost:9999");
-//        var loginPage = new LoginPage();
-//        var authInfo = DataHelper.getAuthInfo();
-//        var verificationPage = loginPage.validLogin(authInfo);
-//        var verificationCode = DataHelper.getVerificationCodeFor(authInfo); //получаем VerificationCode
-//        verificationPage.validVerify(verificationCode); //на verificationPage вводим код
-//        var transferPage = new TransferPage();
-//        transferPage.replenishInSecond();
-//    }
-//
-//    @Test
-//    void shouldTransferMoneyBetweenOwnCardsV2() {
-//        open("http://localhost:9999");
-//        var loginPage = new LoginPage(); //здесь мы инициализируем через конструктор
-////    var loginPage = open("http://localhost:9999", LoginPageV2.class);
-//        var authInfo = DataHelper.getAuthInfo();
-//        var verificationPage = loginPage.validLogin(authInfo);
-//        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-//        verificationPage.validVerify(verificationCode);
+//    void shouldGetErrorMessageIfAmountMoreBalance(){
+//        var firstCardInfo = getFirstCardInfo();
+//        var secondCardInfo = getSecondCardInfo();
+//        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+//        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+//        var amount = generateInvalidAmount(firstCardBalance);
+//        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+//        transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+//        transferPage.findErrorMessage("Не хватает денег для перевода");
+//        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+//        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+//        assertEquals(firstCardBalance, actualBalanceFirstCard);
+//        assertEquals(secondCardBalance, actualBalanceSecondCard);
 //    }
 }
